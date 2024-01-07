e057cc58abb5a0c6e16ba1d906feff6909df9875
astakhov.oleg@gmail.com <Astakhov>
2024-01-07 20:17:42 +0300

    add main.py, recall summ

--- funcs.py
+++ funcs.py
@@ -4,9 +4,3 @@ def summ(x , y):
 def subtract(x , y):
   return x-y
 
-
-
-x = int(input('input x:'))
-y = int(input('input y:'))
-print(summ(x,y))
-print(subtract(x,y))
---
+++ main.py
@@ -0,0 +1,5 @@
+from funcs import summ
+
+x = int(input('input x: '))
+y = int(input('input y: '))
+print(summ(x, y))
